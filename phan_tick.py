3eeb4781e21608c5be2e6d784ca37bbecef78404
leuduc2002 <leuminhduc2002@gmail.com>
2018-06-22 17:23:05 +0700

    session 6

---
+++ phan_tick.py
@@ -0,0 +1,24 @@
+Object 1: Snake
+Attributes:
+x: int
+y: int
+Method 1 :
+Draw(): None
+Move(): None
+
+Object 2:Food
+Attributes:
+x: int
+y: int
+Method 2:
+Draw: None
+
+Object 3: Game
+Attributes 3:
+Snake(pygame.draw.rect(display_sur))
+Food(pos): int
+Method 3:
+Draw()
+Random food(): x pos, y pos
+
+
